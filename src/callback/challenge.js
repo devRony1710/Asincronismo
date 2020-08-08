278ddea7fe44a2b367deffeba40e187328d3d8f6
Rony Antolinez <rony1710.dev@gmail.com>
2020-08-07 20:30:17 -0500

    practice

---
+++ src/callback/challenge.js
@@ -0,0 +1,15 @@
+const fetchData = require('../utils/fetchData')
+const API = 'https://rickandmortyapi.com/api/character/'
+
+fetchData(API, function(error1, data1){
+    if(error1) return console.error(error1)
+        fetchData(API + data1.results[0].id, function(error2, data2){
+            if(error2) return console.error(error2)
+                fetchData(data2.origin.url, function(error3, data3){
+                    if(error3) return console.error(error3)
+                        console.log(data1.info.count)
+                        console.log(data2.name)
+                        console.log(data3.dimension)
+                })
+        })
+})
